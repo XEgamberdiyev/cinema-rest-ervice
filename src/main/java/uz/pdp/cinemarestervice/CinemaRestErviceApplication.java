00bfd33db230f68cf2a03b9d1604ce87b116750f
Egamberdiyev Xayitboy <xayitbekegamberdiyev@gmail.com>
2022-03-14 15:13:07 +0500

    Cinema rest project

---
+++ src/main/java/uz/pdp/cinemarestervice/CinemaRestErviceApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.cinemarestervice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaRestErviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaRestErviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/cinemarestervice/CinemaRestErviceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.cinemarestervice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaRestErviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
